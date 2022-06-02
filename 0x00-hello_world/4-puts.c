bca6a467733b31a7d016ac7e6a14f57692e8a00d
Rethabile1312 <sakwane98@gmail.com>
2022-06-02 16:24:20 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Return Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("\"Programming is like building a multiligual puzzle");
+	puts("\"Programming is like building a multiligual puzzle");
 	return (0);
 }
