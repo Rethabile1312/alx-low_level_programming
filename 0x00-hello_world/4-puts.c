7d579c13385fe75a7911faabf63eb76bc7398083
Rethabile1312 <sakwane98@gmail.com>
2022-06-02 15:14:52 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <studio.h>
+/**
+ *  main - beg
+ *
+ *  Return: stop
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
