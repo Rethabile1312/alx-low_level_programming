517fbaa77ea4b85f339cd3de0e74748a6ff6c86e
Rethabile1312 <sakwane98@gmail.com>
2022-06-02 15:49:29 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <studio.h>
+
 /**
- *  main - beg
+ * main - Entry point
  *
- *  Return: stop
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("\"Programming is like building a multiligual puzzle");
 	return (0);
 }
