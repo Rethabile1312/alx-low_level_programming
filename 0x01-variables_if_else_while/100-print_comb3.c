58259835c0c5b1e974677aecd6c8e7e3350a3d1e
Rethabile1312 <sakwane98@gmail.com>
2022-06-03 17:56:26 -0700

    Ree

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,12 +8,12 @@ int main(void)
 	int i;
 	int j;
 
-	for (i =48; i < 58; i++)
+	for (i = 48; i < 58; i++)
 	for (i = 48; i < 58; i++)
 	{
 		for (j = 48; j < 58; j++)
 		{
-			if(i != j && i < j)
+			if (i != j && i < j)
 			if (i != j && i < j)
 			{
 				putchar(i);
