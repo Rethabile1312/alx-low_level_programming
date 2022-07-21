dc4f230485d6a485a5776f466d2481cf3e0ea552
Rethabile1312 <sakwane98@gmail.com>
2022-07-21 16:22:34 -0700

    i

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,6 +6,7 @@
  *
  * Return: unsigned int.
  */
+
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int ui;
@@ -18,6 +19,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (len = 0; b[len] != '\0'; len++)
 		;
+
 
 	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
